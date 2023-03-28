968cfeb5cf2676984f0b0fe2eec43da8ccf69b8b
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-03-28 16:59:30 -0500

    set window config file

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -4,15 +4,21 @@ from src.ecs.systems.s_rendering import system_rendering
 from src.ecs.systems.s_movement import system_movement
 import esper
 import pygame
+import json
 
+window_config_file = json.load(open('./assets/cfg/window.json'))
+window_size = window_config_file['window']['size']
+framerate = window_config_file['window']['framerate']
+bg_color = window_config_file['window']['bg_color']
 
 class GameEngine:
     def __init__(self) -> None:
         pygame.init()
-        self.screen = pygame.display.set_mode((640,360),pygame.SCALED)
+        self.screen = pygame.display.set_mode((window_size['w'],window_size['h']),pygame.SCALED)
+        pygame.display.set_caption(window_config_file['window']['title'])
         self.clock = pygame.time.Clock()
         self.is_running = False
-        self.framerate = 60
+        self.framerate = framerate
         self.delta_time = 0
 
         self.ecs_world = esper.World()
@@ -48,7 +54,10 @@ class GameEngine:
         system_screen_bounce(self.ecs_world, self.screen)
 
     def _draw(self):
-        self.screen.fill((0,200,154))
+        self.screen.fill((bg_color['r'],
+                          bg_color['g'],
+                          bg_color['b']))
+
         system_rendering(self.ecs_world, self.screen)
         
 
